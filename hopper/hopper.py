f62d77730e88277a62db8d03b901c1a5f7d5bb71
joehahn <jmh.datasciences@gmail.com>
2018-02-05 16:27:28 -0600

    hopper

--- hopper/hopper.py
+++ hopper/hopper.py
@@ -15,9 +15,9 @@ import random
 import copy
 
 #initialize the environment = dict containing all constants that describe the system
-def initialize_environment(rn_seed, max_moves):
+def initialize_environment(rn_seed, cat_strategy, max_moves):
     random.seed(rn_seed)
-    environment = {'rn_seed':rn_seed, 'max_moves':max_moves}
+    environment = {'rn_seed':rn_seed, 'cat_strategy':cat_strategy, 'max_moves':max_moves}
     return environment
 
 #initialize state with bug at origin and cat randomly placed
@@ -39,37 +39,46 @@ def get_separation(state):
     return separation
 
 #move the bug
-def move_bug(bug_xy, cat_xy):
+def move_bug(environment, state):
     #random component of bug's movement
     stdev = 1.0
-    distance = random.normalvariate(0.0, stdev)
+    random_distance = random.normalvariate(0.0, stdev)
     pi = np.pi
-    direction_angle = random.uniform(-pi, pi)
-    bug_dx = distance*np.cos(direction_angle)
-    bug_dy = distance*np.sin(direction_angle)
-    #system component is away from cat
-    delta_x = cat_xy[0] - bug_xy[0]
-    delta_y = cat_xy[0] - bug_xy[0]
-    delta = np.sqrt(delta_x**2 + delta_y**2)
-    distance = 1.0/delta
-    distance_max = 3.33
-    if (distance > distance_max):
-        distance = distance_max
-    direction_angle = np.arctan2(delta_y, delta_x)
-    bug_dx += distance*np.cos(direction_angle)
-    bug_dy += distance*np.sin(direction_angle)
+    random_angle = random.uniform(-pi, pi)
+    bug_dx = random_distance*np.cos(random_angle)
+    bug_dy = random_distance*np.sin(random_angle)
+    #systematic component is away from cat
+    bug_xy = state['bug_xy']
+    cat_xy = state['cat_xy']
+    delta = -(cat_xy - bug_xy)
+    delta_x = delta[0]
+    delta_y = delta[1]
+    separation = np.sqrt(delta_x**2 + delta_y**2)
+    systematic_distance = 1.0/np.sqrt((1/3.33)**2 + separation**2)
+    systematic_angle = np.arctan2(delta_y, delta_x)
+    bug_dx += systematic_distance*np.cos(systematic_angle)
+    bug_dy += systematic_distance*np.sin(systematic_angle)
     bug_delta_xy = np.array([bug_dx, bug_dy])
     return bug_delta_xy
 
-#move bug and cat
-def update_state(state, cat_delta_xy):
+#move cat
+def move_cat(environment, state):
+    cat_strategy = environment['cat_strategy']
     bug_xy = state['bug_xy'].copy()
     cat_xy = state['cat_xy'].copy()
-    bug_delta_xy = move_bug(bug_xy, cat_xy)
+    if (cat_strategy == 'simple'):
+        #cat leaps right at bug
+        cat_delta_xy = bug_xy - cat_xy
+    return cat_delta_xy
+
+#move bug and cat
+def update_state(environment, state):
+    bug_delta_xy = move_bug(environment, state)
+    cat_delta_xy = move_cat(environment, state)
     next_state = copy.deepcopy(state)
     next_state['bug_xy'] += bug_delta_xy
     next_state['cat_xy'] += cat_delta_xy
-    return next_state
+    return next_state, cat_delta_xy
 
 #calculate reward = 1/(bug-cat separation)
 def get_reward(state):
